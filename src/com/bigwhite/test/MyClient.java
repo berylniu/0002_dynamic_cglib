170bf5d2c0dbe01f85a9eff39a04abecab9dcc5e
beryl <nylovewdm@163.com>
2018-09-27 09:29:47 +0800

    步骤分析

--- src/com/bigwhite/test/MyClient.java
+++ src/com/bigwhite/test/MyClient.java
@@ -7,7 +7,12 @@ import com.bigwhite.service.ServiceEntrust;
 import com.bigwhite.service.SomeServiceImpl;
 
 public class MyClient {
-	
+	/*步骤
+	 * 1、目标类必须实现业务接口
+	 * 2、用的为类Proxy.newProxyInstance()
+	 * 3、客户类中需有目标对象
+	 * 4、需要定义一个实现invocationhandler的接口
+	 * */
 	public static void main(String[] args) {
 		
 //	传说中的目标对象target
